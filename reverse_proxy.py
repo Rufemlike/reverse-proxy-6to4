fb81871cc4fbff8be1fbc67e5b485e86d26d1ad8
Rufemlike <99505088+Rufemlike@users.noreply.github.com>
2025-12-11 00:07:00 +0300

    Update reverse_proxy.py

--- reverse_proxy.py
+++ reverse_proxy.py
@@ -1,7 +1,6 @@
 """
 Copyright 2022 GamingCoookie
-Modded by Rufemlike
-
+Copyright 2025 Rufemlike
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
